30f1d4ddbe213d577e839aa9ecb49b7bb6772b0a
LAONE MARIO POLAO <fcse24-008@thuto.bac.ac.bw>
2025-11-17 05:30:17 +0000

    Save my Codespaces changes

---
+++ src/main/java/com/laone/customers/IndividualCustomer.java
@@ -0,0 +1,41 @@
+package com.laone.customers;
+import com.laone.system.DBConnector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class IndividualCustomer extends Customer
+{
+    private String pin;
+
+    public String getPin() {return pin;}
+    public void setPin(String pin) {this.pin = pin;}
+
+    @Override
+    public String toString() {
+                                 return "IndividualCustomer = " + "customerID: " + getCustomerID() + '\'' + ", firstname: " + getFirstname() + '\'' + ", lastname: " + getLastname() + '\'' + ", contact: " + getContact() + '\'' + ", nationalID: " + getNationalID() + '\'' + ", gender: " + getGender() + '}';
+                             }
+
+    public void saveToDatabase()
+    {
+        Connection conn = DBConnector.connect();
+        if (conn != null) {
+            try {
+                PreparedStatement stmt = conn.prepareStatement(
+                        "INSERT INTO individual_customer (customerID, firstname, lastname , gender, contact, nationalID, pin) VALUES (?, ?, ?, ?, ?, ?, ?)"
+                );
+                stmt.setInt(1, getCustomerID());
+                stmt.setString(2, getFirstname());
+                stmt.setString(3, getLastname());
+                stmt.setString(4, getGender());
+                stmt.setString(5, getContact());
+                stmt.setInt(6, getNationalID());
+                stmt.setString(7, this.pin);
+                stmt.executeUpdate();
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("Customer DB error: " + e.getMessage());
+            }
+        }
+    }
+}
